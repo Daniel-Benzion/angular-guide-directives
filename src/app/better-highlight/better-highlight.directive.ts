9176e03f62aa06affd3605af85178fb7e7d9c4ce
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-09-30 18:05:33 -0500

    Binding to directive properties

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,33 +1,38 @@
-import { Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
 
-  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
-  @HostBinding('style.color') color: string = 'black';
+  @Input() defaultBackground: string = 'transparent';
+  @Input() highlightBackground: string = 'rgb(0, 132, 255)';
+  @Input() defaultColor: string = 'black';
+  @Input() highlightColor: string = 'white';
+
+  @HostBinding('style.backgroundColor') backgroundColor: string;
+  @HostBinding('style.color') color: string = this.defaultColor;
 
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
-    
+    this.backgroundColor = this.defaultBackground;
   }
 
   @HostListener('mouseenter') mouseenter(eventData: Event) {
     // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'rgb(0, 132, 255)');
     // this.renderer.setStyle(this.elRef.nativeElement, 'color', 'white');
 
-    this.backgroundColor = 'rgb(0, 132, 255)';
-    this.color = 'white';
+    this.backgroundColor = this.highlightBackground;
+    this.color = this.highlightColor;
   }
 
   @HostListener('mouseleave') mouseleave(eventData: Event) {
     // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
     // this.renderer.setStyle(this.elRef.nativeElement, 'color', 'black');
 
-    this.backgroundColor = 'transparent';
-    this.color = 'black';
+    this.backgroundColor = this.defaultBackground;
+    this.color = this.defaultColor;
   }
 
 }
